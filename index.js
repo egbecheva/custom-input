c430c9fbea0c2b3e120a1f949e1475add530f901
E.Becheva <34000861+egbecheva@users.noreply.github.com>
2023-02-15 18:19:45 +0200

    wip:handle arrow keys

--- index.js
+++ index.js
@@ -1,38 +1,83 @@
-let optionsArray = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+const optionsArray = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30,
 ];
-const maxOptions = 10;
-let finalArray = [];
+const optionsArrayLength = optionsArray.length;
+
+const MAX_OPTIONS = 10;
+const input = document.getElementById('inputField');
+const dropdownContent = document.getElementById('dropdown-content');
+
+let finalDropdownOptions = [];
+
+//Map through all items and render only the last ten of them
 let listItems = optionsArray.map((element, index) => {
-  if (index <= maxOptions) {
-    finalArray = [
-      ...finalArray,
+  let lastItems = optionsArrayLength - index;
+  if (lastItems <= MAX_OPTIONS) {
+    finalDropdownOptions = [
+      ...finalDropdownOptions,
+      `<li value=${element} class="displayed dropdown-item"> ${element} </li>`,
+    ];
+  }
+  if (lastItems > MAX_OPTIONS) {
+    finalDropdownOptions = [
+      ...finalDropdownOptions,
       `<li value=${element} class="not-displayed dropdown-item"> ${element} </li>`,
     ];
   }
-  if (index > maxOptions) {
-    finalArray = [
-      ...finalArray,
-      `<li value=${element} class="dropdown-item"> ${element} </li>`,
-    ];
-  }
 
-  return finalArray;
+  return finalDropdownOptions;
 });
-const htmlList = finalArray.join('');
+
+const htmlList = finalDropdownOptions.join('');
 const appendInputOptions = (arr) => {
-  document.getElementById('dropdown-content').innerHTML += arr;
+  dropdownContent.innerHTML += arr;
 };
+
+//Add options to html file
 appendInputOptions(htmlList);
 
-const handleMouseOver = (element) => {
-  const input = document.getElementById('inputField');
-  const selectedValue = element.getAttribute('value');
+//Update input value with the selected one from the dropdown list
+const handleMouseOver = (element, i, arr) => {
+  let selectedValue = element.getAttribute('value');
   input.value = selectedValue;
 };
+const dropdownList = document.querySelectorAll('.dropdown-item');
 
-const dropDownList = document.querySelectorAll('.dropdown-item');
-
-dropDownList.forEach((element) =>
-  element.addEventListener('mouseover', () => handleMouseOver(element))
+dropdownList.forEach((element, i, arr) =>
+  element.addEventListener('mouseover', () => handleMouseOver(element, i, arr))
 );
+
+const displayedOptions = Array.from(
+  document.querySelectorAll('.displayed', console.log())
+);
+
+//Handle arrow keys
+const handleArrowKeys = () => {
+  let firsAvailableIndex = optionsArrayLength - MAX_OPTIONS;
+  let i = firsAvailableIndex;
+  console.log('displayedOptions', displayedOptions);
+
+  document.onkeydown = (event) => {
+    let selectedValue = finalDropdownOptions[firsAvailableIndex];
+    if (event.key === 'ArrowUp') {
+      console.log('up');
+      i = i - 1;
+      console.log('i', i);
+
+      selectedValue = finalDropdownOptions[i];
+      input.value = selectedValue;
+    }
+    if (event.key === 'ArrowDown') {
+      console.log('down');
+
+      i = i + 1;
+      console.log('i', i);
+
+      selectedValue = finalDropdownOptions[i];
+      input.value = selectedValue;
+    }
+  };
+};
+
+input.addEventListener('focus', () => handleArrowKeys());
